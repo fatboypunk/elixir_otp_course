e8a35922c44141f544854159bc57d64d11ffab21
Marcel Horlings <marcel.horlings@gmail.com>
2018-08-04 22:33:28 +0200

    let the words count use streams to improve the memory usage

--- words/lib/words.ex
+++ words/lib/words.ex
@@ -2,20 +2,12 @@ defmodule Words do
   @doc """
   Counts the numbor of words on a list of linex
 
-  ## Examples
-
-      iex(0)> Words.count(["a,", "a a a", "b a"])
-      %{"a" => 5, "b" => 1}
-
-      iex(0)> Words.count(["a,", "a & a", "b a"])
-      %{"a" => 4, "b" => 1}
-
   """
-  def count([_ | _] = lines) do
+  def count(lines, mod) do
     lines
-    |> Enum.flat_map(&String.split/1)
-    |> Enum.map(&String.downcase/1)
-    |> Enum.map(&remove_special_chars/1)
+    |> mod.flat_map(&String.split/1)
+    |> mod.map(&String.downcase/1)
+    |> mod.map(&remove_special_chars/1)
     |> Enum.reduce(%{}, &count_word/2)
   end
 
--- words/test/words_test.exs
+++ words/test/words_test.exs
@@ -1,4 +1,22 @@
 defmodule WordsTest do
   use ExUnit.Case
   doctest Words
+
+  test "count the numbor of words on a list of line" do
+    assert %{
+             "annihilated" => 1,
+             "sedateness" => 1,
+             "citizens" => 6,
+             "roots" => 1,
+             "dissect" => 1,
+             "destroys" => 2,
+             "attempting" => 2,
+             "reminder" => 4,
+             "supervision" => 1,
+             "ledge" => 4,
+             "bandy" => 3,
+             "handleless" => 1,
+             "onelet" => 1
+           } = Words.count(File.stream!("test/support/files/war_and_peace.txt"), Stream)
+  end
 end
